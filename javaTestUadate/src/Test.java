f7275c2fd640b75c39fff7004dc55d2464262d66
KobeBryant <kobe.zhou@126.com>
2021-01-23 01:16:48 +0800

    第一次测试提交

---
+++ javaTestUadate/src/Test.java
@@ -0,0 +1,9 @@
+/**
+ * @ahthor Kobe
+ * @Create 2021-01-23 1:15 上午
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
---
+++ javaTestUadate/src/com/test/Test1.java
@@ -0,0 +1,8 @@
+package com.test;
+
+/**
+ * @ahthor Kobe
+ * @Create 2021-01-23 1:16 上午
+ */
+public class Test1 {
+}
